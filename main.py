dc31d3dadca4f0010102e8a640549b13f55aca75
2b-creator <qjtykr65536@gmail.com>
2024-07-15 09:27:58 +0800

    wish you are here

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Apis.ClientApi import *
 from Apis.AdminApi import *
 from Apis.UserApi import *
+from gevent import pywsgi
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    server = pywsgi.WSGIServer(('127.0.0.1', 5000), app)
+    server.serve_forever()
